7db2d64e1517932dfd01fdb603c94f7b81afe0d5
garyngo <52193187+garyngo@users.noreply.github.com>
2023-07-02 16:12:29 +0700

    added first step of solution

---
+++ foundations/javascript/Rock Paper Scissors/script.js
@@ -0,0 +1,10 @@
+let choice = ['Rock', 'Paper', 'Scissors'];
+let randomchoice = Math.floor(Math.random() * choice.length);
+
+
+function getComputerChoice(randomchoice) {
+    return 
+}
+
+
+console.log(randomchoice, choice[randomchoice])
